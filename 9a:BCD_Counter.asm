75bdabd26a92528fba0ff11e36cdb8e2993ba261
Vikas NS <nsvikas1998@gmail.com>
2018-06-05 17:48:46 +0530

    Create 9a:BCD_Counter.asm

---
+++ 9a:BCD_Counter.asm
@@ -0,0 +1,36 @@
+.model tiny
+.code
+	start:
+		mov bh,00h
+		mov bl,00h
+		
+		mov ah,02h
+		mov cx,100
+		next:
+			mov al,bl
+			aam
+			add ax,3030h
+			
+			push ax
+			
+			mov dl,ah
+			mov ah,02h
+			int 21h
+			
+		    pop ax
+			
+			mov dl,al
+			mov ah,02h
+			int 21h
+			
+			mov dl," "
+			int 21h
+			
+			inc bl	
+			loop next
+		mov ah,4ch
+		int 21h
+		
+
+		
+	end start
